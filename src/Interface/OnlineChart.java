8ed904c78c36a8b7a7640b8813713d5f0a9e00bf
lenguyenthai123 <lnt0995449235@gmail.com>
2023-12-24 15:38:42 +0700

    User Online chart call API

---
+++ src/Interface/OnlineChart.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Interface;
+
+import com.google.gson.annotations.SerializedName;
+
+/**
+ *
+ * @author lnt09
+ */
+public class OnlineChart {
+    public int month;
+    
+    @SerializedName("number_of_user")
+    public int number;
+}
--- src/components/UserOnlineChart/FormUserOnlineChart.java
+++ src/components/UserOnlineChart/FormUserOnlineChart.java
@@ -4,9 +4,25 @@
  */
 package components.UserOnlineChart;
 
+import Interface.OnlineChart;
+import com.google.gson.Gson;
 import components.RegisteredChart.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.util.ArrayList;
 import java.util.List;
+import java.net.http.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.SwingWorker;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 
 public class FormUserOnlineChart extends javax.swing.JPanel {
 
@@ -18,21 +34,105 @@ public class FormUserOnlineChart extends javax.swing.JPanel {
     }
 
     public void initData() {
-        List<ModelRegisteredChartLine> list = new ArrayList<>();
-        list.add(new ModelRegisteredChartLine("Friday", 125));
+//        List<ModelRegisteredChartLine> list = new ArrayList<>();
+//        list.add(new ModelRegisteredChartLine("Friday", 125));
+//
+//        list.add(new ModelRegisteredChartLine("Monday", 10));
+//        list.add(new ModelRegisteredChartLine("Tuesday", 150));
+//        list.add(new ModelRegisteredChartLine("Wednesday", 80));
+//        list.add(new ModelRegisteredChartLine("Thursday", 100));
+//        list.add(new ModelRegisteredChartLine("Friday", 125));
+//        list.add(new ModelRegisteredChartLine("Saturday", 80));
+//        list.add(new ModelRegisteredChartLine("Sunday", 200));
+//        list.add(new ModelRegisteredChartLine("Saturday", 80));
+//        list.add(new ModelRegisteredChartLine("Sunday", 200));
+//        list.add(new ModelRegisteredChartLine("Saturday", 80));
+//        list.add(new ModelRegisteredChartLine("Sunday", 200));
+//        userOnlineChartLine.setModel(list);
 
-        list.add(new ModelRegisteredChartLine("Monday", 10));
-        list.add(new ModelRegisteredChartLine("Tuesday", 150));
-        list.add(new ModelRegisteredChartLine("Wednesday", 80));
-        list.add(new ModelRegisteredChartLine("Thursday", 100));
-        list.add(new ModelRegisteredChartLine("Friday", 125));
-        list.add(new ModelRegisteredChartLine("Saturday", 80));
-        list.add(new ModelRegisteredChartLine("Sunday", 200));
-        list.add(new ModelRegisteredChartLine("Saturday", 80));
-        list.add(new ModelRegisteredChartLine("Sunday", 200));
-        list.add(new ModelRegisteredChartLine("Saturday", 80));
-        list.add(new ModelRegisteredChartLine("Sunday", 200));
-        userOnlineChartLine.setModel(list);
+        new CallAPIUserOnlineChart().execute();
+        userOnlineChartSearching.addListenerSearchButton(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                new CallAPIUserOnlineChart().execute();
+            }
+        });
+    }
+
+    private class CallAPIUserOnlineChart extends SwingWorker<String, OnlineChart[]> {
+
+        @Override
+        protected String doInBackground() {
+            try {
+                String year = userOnlineChartSearching.getYear();
+                String api = "http://13.215.176.178:8881/admin/users-active-each-month" + "?year=" + year;
+
+                HttpClient client = HttpClient.newHttpClient();
+
+                HttpRequest req = HttpRequest.newBuilder()
+                        .uri(new URI(api))
+                        .GET()
+                        .build();
+
+                HttpResponse<String> res = client.send(req, HttpResponse.BodyHandlers.ofString());
+
+                int resCode = res.statusCode();
+                String body = res.body();
+
+                if (resCode == HttpURLConnection.HTTP_OK) {
+                    System.out.println("Call API thanh cong");
+
+                    JSONParser par = new JSONParser();
+                    JSONObject data = (JSONObject) par.parse(body);
+                    JSONArray data1 = (JSONArray) data.get("data");
+
+                    String json = data1.toString();
+
+                    Gson gson = new Gson();
+
+                    OnlineChart[] list = gson.fromJson(json, OnlineChart[].class);
+
+                    publish(list);
+                } else {
+                    System.out.println("Call API that bai");
+
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            } catch (URISyntaxException ex) {
+                Logger.getLogger(FormUserOnlineChart.class.getName()).log(Level.SEVERE, null, ex);
+            } catch (InterruptedException ex) {
+                Logger.getLogger(FormUserOnlineChart.class.getName()).log(Level.SEVERE, null, ex);
+            } catch (ParseException ex) {
+                Logger.getLogger(FormUserOnlineChart.class.getName()).log(Level.SEVERE, null, ex);
+            }
+            return "Failed";
+        }
+
+        @Override
+        protected void process(List<OnlineChart[]> chunks) {
+
+            List<ModelRegisteredChartLine> list = new ArrayList<>();
+
+            int[] month = new int[13];
+
+            for (int i = 1; i <= 12; i++) {
+                month[i] = 0;
+            }
+
+            OnlineChart[] data = chunks.get(chunks.size() - 1);
+
+            for (OnlineChart chart : data) {
+                System.out.println("Month: " + chart.month + " User: " + chart.number);
+                month[chart.month] = chart.number;
+            }
+
+            for (int i = 1; i <= 12; i++) {
+                list.add(new ModelRegisteredChartLine("Nothing", month[i]));
+            }
+            userOnlineChartLine.setModel(list);
+
+        }
     }
 
     @SuppressWarnings("unchecked")
@@ -40,7 +140,7 @@ public class FormUserOnlineChart extends javax.swing.JPanel {
     private void initComponents() {
 
         jLabel1 = new javax.swing.JLabel();
-        userOnlineChartSearching2 = new components.UserOnlineChart.UserOnlineChartSearching();
+        userOnlineChartSearching = new components.UserOnlineChart.UserOnlineChartSearching();
         userOnlineChartLine = new components.UserOnlineChart.UserOnlineChartLine();
 
         setBackground(new java.awt.Color(250, 250, 250));
@@ -61,7 +161,7 @@ public class FormUserOnlineChart extends javax.swing.JPanel {
                     .addGroup(layout.createSequentialGroup()
                         .addGap(23, 23, 23)
                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
-                            .addComponent(userOnlineChartSearching2, javax.swing.GroupLayout.PREFERRED_SIZE, 978, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addComponent(userOnlineChartSearching, javax.swing.GroupLayout.PREFERRED_SIZE, 978, javax.swing.GroupLayout.PREFERRED_SIZE)
                             .addGroup(layout.createSequentialGroup()
                                 .addComponent(jLabel1)
                                 .addGap(823, 823, 823)))))
@@ -73,7 +173,7 @@ public class FormUserOnlineChart extends javax.swing.JPanel {
                 .addGap(17, 17, 17)
                 .addComponent(jLabel1)
                 .addGap(18, 18, 18)
-                .addComponent(userOnlineChartSearching2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addComponent(userOnlineChartSearching, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addComponent(userOnlineChartLine, javax.swing.GroupLayout.PREFERRED_SIZE, 662, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addContainerGap(56, Short.MAX_VALUE))
@@ -84,6 +184,6 @@ public class FormUserOnlineChart extends javax.swing.JPanel {
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JLabel jLabel1;
     private components.UserOnlineChart.UserOnlineChartLine userOnlineChartLine;
-    private components.UserOnlineChart.UserOnlineChartSearching userOnlineChartSearching2;
+    private components.UserOnlineChart.UserOnlineChartSearching userOnlineChartSearching;
     // End of variables declaration//GEN-END:variables
 }
